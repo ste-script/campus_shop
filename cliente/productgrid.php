714d1bd585989d6fc1ca2ba29167fd6b6aa03563
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-12-16 00:10:11 +0100

    Create grid and correct carousel phantom img

---
+++ cliente/productgrid.php
@@ -0,0 +1,48 @@
+<?php require_once("../bootstrap.php");
+require_once("../layouts/headerCostumer.php");
+//mobile 6 col-xl-3 colonne, desktop 8
+$prod = $dbh->getProductsFromCategories("ufficio");
+$prod = array_merge($prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod, $prod);
+$index = 0;
+?>
+
+<div class="container-xl">
+  <?php while ($index < count($prod)) : ?>
+    <div class="row">
+      <div class="col-6">
+        <div class="row">
+          <?php
+          for ($i = $index; $i < count($prod) && $i < $index + 4; $i++) : ?>
+            <div class="col-xs-6 col-xl-3">
+              <button class="btn btn-default">
+                <?php echo $dbh->getImgFromId($prod[$i]["id"]) . "width='50%'/>"; ?>
+                <h5> <?php echo  $prod[$i]["nome"]; ?></h5>
+                <h5> <?php echo "€" . $prod[$i]["prezzo"]; ?></h5>
+              </button>
+            </div>
+          <?php
+          endfor;
+          $index = $i;
+          ?>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="row">
+          <?php
+          for ($i = $index; $i < count($prod) && $i < $index + 4; $i++) : ?>
+            <div class="col-xs-6 col-xl-3">
+              <button class="btn btn-default">
+                <?php echo $dbh->getImgFromId($prod[$i]["id"]) . "width='50%'/>"; ?>
+                <h5> <?php echo  $prod[$i]["nome"]; ?></h5>
+                <h5> <?php echo "€" . $prod[$i]["prezzo"]; ?></h5>
+              </button>
+            </div>
+          <?php
+          endfor;
+          $index = $i;
+          ?>
+        </div>
+      </div>
+    </div>
+  <?php endwhile ?>
+</div>

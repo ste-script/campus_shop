e1ce218739e72a15427937323b974a06eb5342b0
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-12-16 10:11:55 +0100

    fix bootstrap

--- layouts/headerCostumer.php
+++ layouts/headerCostumer.php
@@ -1,12 +1,13 @@
 <body>
     <!-- BAR-->
     <nav class="navbar sticky-top bg-dark navbar-dark">
+    <div class="container-fluid">
 
     <!-- Toggler/collapsibe Button -->
-        <div class="dropdownMenu">
-            <button class="btn btn-link dropdown" data-toggle="dropdown">
-                <img src=".\img\logo.png" alt="Menu" style="width:50px;">
-            </button>
+        <div class="dropdown nav-item">
+        <a class="nav-link dropdown-toggle" href="#" id="navCategoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src=".\img\logo.png" alt="Menu" style="width:50px;">
+          </a>
             <div class="dropdown-menu">
                 <a class="dropdown-item" href="#">Category 1</a>
                 <a class="dropdown-item" href="#">Category 2</a>
@@ -14,24 +15,27 @@
             </div>
         </div>
 
-        <form class="form-inline" action="/action_page.php">
-            <input class="form-control w-auto" type="text" placeholder="Search">
-            <button class="btn" type="submit">
+
+        <form class="d-flex" action="/action_page.php">
+            <input class="form-control me-2 w-auto" type="search" placeholder="Search">
+            <button class="btn " type="submit">
                 <span class="fa fa-search"  style="color:white"></span>
             </button>
         </form>
 
-        <div class="dropdownMenu">
-            <button class="btn btn-link dropdown" data-toggle="dropdown">
+
+        <div class="dropdown nav-item">
+        <a class="nav-link" href="#" id="navMenuDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="navbar-toggler-icon" alt="UserMenu" ></span>
-            </button>
-            <div class="dropdown-menu dropdown-menu-right">
+        </a>
+            <div class="dropdown-menu end-0">
                 <a class="dropdown-item" href="#">Cart</a>
                 <a class="dropdown-item" href="#">Orders</a>
                 <a class="dropdown-item" href="#">Card</a>
                 <a class="dropdown-item" href="#">Account</a>
             </div>
         </div>
+    </div>
     </nav>
 
     <!-- /BAR-->

b5cce023d03e98a05bb8cdc69d3794a59167bd76
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-12-18 12:02:56 +0100

    footer accessibility

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,24 +1,9 @@
 <footer class="bg-dark text-center text-white ">
   <!-- Grid container -->
   <div class="container p-3 mb-1 ">
-    <!-- Facebook -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
-
-    <!-- Twitter -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
-
-    <!-- Google -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
-
-    <!-- Instagram -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
-
-    <!-- Linkedin -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
-
     <!-- Github -->
-    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
-  
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" title="Github link"><strong class="fab fa-github"></strong></a>
+
   </div>
   <!-- Grid container -->
 

a0df60f0a3a93fdade9be39a815f4bd519d2a1c1
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-12-29 11:20:54 +0100

    insert new prosucts title and fix carousel

--- cliente/carouselItems.php
+++ cliente/carouselItems.php
@@ -4,7 +4,7 @@ for ($i = $index; $i < count($prod) && $i < $index + CAROUSEL_ITEM_NUMBER; $i++)
     <a class=" btn btn-default align-top" href="product.php?productId=<?php echo $prod[$i]["id"] ?>">
         <?php echo $dbh->getImgFromId($prod[$i]["id"]) . "/>"; ?>
         <div class="text-capitalize fs-6 pt-3">
-            <h5> <?php echo  $prod[$i]["nome"]; ?></h5>
+            <h5 class="d-none d-md-block"> <?php echo  $prod[$i]["nome"]; ?></h5>
             <h5 class="fw-bold"> <?php echo "€" . $prod[$i]["prezzo"]; ?></h5>
         </div>
     </a>

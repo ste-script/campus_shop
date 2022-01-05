e0bff18d422c2899720173f3064ca2bea037e917
Lorenzo <lorenzo.bombardini@studio.unibo.it>
2022-01-05 17:53:35 +0100

    add no search result page

--- search.php
+++ search.php
@@ -4,14 +4,12 @@ if (!isset($_GET["productName"]) || !is_string($_GET["productName"])) {
     header("Location: index.php");
     exit;
 }
-$prod = $dbh->getProductsByName($_GET["productName"]);
+
 $templateParams["titolo"] = "Ricerca di " . $_GET["productName"];
 include('./layouts/header.php');
 
-$templateParams["gridTitle"] = "Ricerca di " . $_GET["productName"];
-$templateParams["products"] = $prod;
-if (!empty($prod)) {
-    include('./cliente/productgrid.php');
-}
+$templateParams["products"] = $dbh->getProductsByName($_GET["productName"]);
+$templateParams['products'] ? $templateParams['gridTitle'] = "Ricerca di " . $_GET["productName"] : $templateParams['gridTitle'] = "Nessun Prodotto";
 
+include('./cliente/productgrid.php');
 include('./layouts/footer.php');

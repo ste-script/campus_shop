0b9a69730dca2b5c3ec04de534b4057063d82a8d
Lorenzo <lorenzo.bombardini@studio.unibo.it>
2022-01-05 18:08:49 +0100

    set required input

--- layouts/register-form.php
+++ layouts/register-form.php
@@ -14,16 +14,16 @@
             </div>
             <div class="mb-3">
                 <label for="registerEmail" class="form-label">Email</label>
-                <input type="email" class="form-control" id="registerEmail" name="registerEmail">
+                <input type="email" class="form-control" id="registerEmail" name="registerEmail" required>
             </div>
             <div class="mb-3">
                 <label for="cf" class="form-label" id="cfLabel">Codice fiscale</label>
-                <input type="text" class="form-control" id="cf" name="cf" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" required="required">
+                <input type="text" class="form-control" id="cf" name="cf" pattern="^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" id="password" minlength="4" name="password" autocomplete="on">
+                    <input type="password" class="form-control" id="password" minlength="4" name="password" autocomplete="on" required>
                     <div class="input-group-append">
                         <button class="btn btn-lg btn-outline-secondary fa fa-eye" type="button" onclick="showPwd(document.getElementById('password'), this)" id="iconPwd"></button>
                     </div>
@@ -32,7 +32,7 @@
             <div class="mb-3">
                 <label for="passwordConfirm" class="form-label">Conferma password</label>
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" id="passwordConfirm" minlength="4" name="passwordConfirm" autocomplete="on">
+                    <input type="password" class="form-control" id="passwordConfirm" minlength="4" name="passwordConfirm" autocomplete="on" required>
                     <div class="input-group-append">
                         <button class="btn btn-lg btn-outline-secondary fa fa-eye" type="button" onclick="showPwd(document.getElementById('passwordConfirm'), this)" id="iconPwd"></button>
                     </div>
@@ -43,13 +43,30 @@
             <button type="submit" id="registerButton" class="btn btn-success">Registrati</button>
         </form>
     </div>
+
+    <!-- Allert Modal -->
+    <div class="modal fade" id="allertModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header p-3">
+                    <h5 class="modal-title text-danger">Errore Durante La Registrazione</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <div class="row my-5 mx-0">
     <div class="col-6 mx-auto">
         <?php
         if (isset($templateParams["erroreLogin"])) {
-            echo "<p class='text-danger'>" . $templateParams["erroreLogin"] . "</p>";
+            echo "<script type='text/javascript'>
+                        $(document).ready(function(){
+                        $('#allertModal').modal('show');
+                        });
+                    </script>";
         }
         ?>
     </div>
